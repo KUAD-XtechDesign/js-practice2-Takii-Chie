df72878fb6714a3ae4436ed6e3d488791c746f4e
Takii_Chie <68629404+Takii-Chie@users.noreply.github.com>
2020-10-23 16:31:13 +0900

    waaaa

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,6 +1,43 @@
 $(function(){
 
+    let txt1 ="冬眠から起こすのは、";
+    let txt2 ="レインボーローズの唇。"
+    let num1 = 10;
+    let num2 = 31;
 
+    function sayHello(){
+        $("#content").text("🦜");
+    }
+
+    function saySomething(txt){
+        $("#content").text(txt);
+    }
+
+    function samTwo(arg1,arg2){
+        $("#content").append(arg1 + arg2);
+    }
+
+    $("#button01").on("click",function(){
+        sayHello();
+    })
+
+    $("#button02").on("click",function(){
+        saySomething(txt1);
+    })
+
+    $("#button03").on("click",function(){
+        samTwo(txt1,txt2);
+    })
+
+    $("#button04").on("click",function(){
+        samTwo(num1,num2);
+    })
+
+    $("#button05").on("click",function(){
+        samTwo(txt1,txt2)
+        samTwo(num1,num2)
+    })
+    
 
 
 })
